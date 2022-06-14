a3f73fe07adf93a1114c7be8c7eb8c6e4acb28a7
Gabe <gtorion97@gmail.com>
2022-06-14 11:09:48 -0500

    refactoring player states

---
+++ src/components/playerInfo/PlayerInfo.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import '../../css/gameSettings/playerInfo.css'
+
+// have the name at the top
+
+// the shape in the middle
+
+const PlayerInfo = () => {
+  return (
+      <div className='playerInfo'>
+          <section>
+        <input/>
+          </section>
+    </div>
+  )
+}
+
+export default PlayerInfo
--- src/provider/SettingsProvider.tsx
+++ src/provider/SettingsProvider.tsx
@@ -1,6 +1,8 @@
 import React, { useState, createContext } from 'react'
 
 import { FC } from 'react';
+import { Player, SettingsVal } from '../interfaces/interfaces';
+import { PlayerState } from '../types/types';
 
 
 export const SettingContext = createContext({});
@@ -9,20 +11,22 @@ interface ProviderProps{
     [key: string]: any
 }
 
-export const SettingProvider:FC<ProviderProps> = props => {
-    const [isSelectedWeatherModalOn, setIsSelectedWeatherModalOn] = useState(false);
-    const [isSearchAndUnitTypesModalOn, setIsSearchAndUnitTypesModalOn] = useState(false);
-    const [isUnitsSelectionModalOn, setIsUnitsSelectionModalOn] = useState(false);
-    const [isSearchTypesModalOn, setIsSearchTypesModalOn] = useState(false);
+export const SettingProvider: FC<ProviderProps> = props => {
+    const getPlayerDefaultVal = (isPlayerOne: Boolean = false) => {
+    const playerDefaultVal:Player = isPlayerOne ? { isSquareChosen: false, name: 'Player 1' } : { isSquareChosen: false, name: 'Player 2' };
+    return playerDefaultVal;
+  }
+  const [player1, setPlayer1]:PlayerState = useState(getPlayerDefaultVal(true));
+  const [player2, setPlayer2]:PlayerState = useState(getPlayerDefaultVal());
+
+    const settingsVal: SettingsVal = {
+        _player1: [player1, setPlayer1],
+        _player2: [player2, setPlayer2]
+  }
 
     return (
         <SettingContext.Provider
-            value={{
-                _isSelectedWeatherModalOn: [isSelectedWeatherModalOn, setIsSelectedWeatherModalOn],
-                _isSearchAndUnitTypesModalOn: [isSearchAndUnitTypesModalOn, setIsSearchAndUnitTypesModalOn],
-                _isUnitsSelectionModalOn: [isUnitsSelectionModalOn, setIsUnitsSelectionModalOn],
-                _isSearchTypesModalOn: [isSearchTypesModalOn, setIsSearchTypesModalOn]
-            }}
+            value={settingsVal}
         >
             {props.children}
         </SettingContext.Provider>
